99b4bcdbb45fef5faa9ada8924353fdcaf1ba720
ngoduongkha <57293030+ngoduongkha@users.noreply.github.com>
2020-10-03 17:50:06 +0700

    add data and class data

---
+++ TuDienAnhViet/DictionaryManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TuDienAnhViet {
+    public class DictionaryManager {
+        private DictionaryItems items;
+        public DictionaryItems Items { get => items; set => items = value; }
+
+        
+    }
+}
