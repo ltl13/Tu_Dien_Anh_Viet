e524fb09d61e17b9676cac8b647890fbf318d351
ltl13 <66907671+ltl13@users.noreply.github.com>
2020-10-15 23:51:15 +0700

    Them hieu ung cho no dep ne

--- TuDienAnhViet/Form_Main.Designer.cs
+++ TuDienAnhViet/Form_Main.Designer.cs
@@ -33,18 +33,13 @@
             this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
             this.metroStyleManager_FormMain = new MetroFramework.Components.MetroStyleManager(this.components);
             this.button_Exit = new System.Windows.Forms.Button();
-            this.metroTile_Exam = new MetroFramework.Controls.MetroTile();
-            this.metroTile_Reading = new MetroFramework.Controls.MetroTile();
-            this.metroTile_Flashcard = new MetroFramework.Controls.MetroTile();
-            this.metroTile_ComboWords = new MetroFramework.Controls.MetroTile();
-            this.metroTile_Grammar = new MetroFramework.Controls.MetroTile();
-            this.metroTile_Search = new MetroFramework.Controls.MetroTile();
-            this.metroPanel_Main = new MetroFramework.Controls.MetroPanel();
-            this.panel_Menu = new System.Windows.Forms.Panel();
-            this.panel_Selection = new System.Windows.Forms.Panel();
-            this.panel_SelectedCursor = new System.Windows.Forms.Panel();
+            this.panel_Main = new System.Windows.Forms.Panel();
+            this.pictureBox_Search = new System.Windows.Forms.PictureBox();
+            this.timer_PanelSearch = new System.Windows.Forms.Timer(this.components);
+            this.panel_Search = new System.Windows.Forms.Panel();
             ((System.ComponentModel.ISupportInitialize)(this.metroStyleManager_FormMain)).BeginInit();
-            this.panel_Menu.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Search)).BeginInit();
+            this.panel_Search.SuspendLayout();
             this.SuspendLayout();
             // 
             // metroStyleManager_FormMain
@@ -68,176 +63,42 @@
             this.button_Exit.MouseEnter += new System.EventHandler(this.button_Exit_MouseEnter);
             this.button_Exit.MouseLeave += new System.EventHandler(this.button_Exit_MouseLeave);
             // 
-            // metroTile_Exam
+            // panel_Main
             // 
-            this.metroTile_Exam.ActiveControl = null;
-            this.metroTile_Exam.Dock = System.Windows.Forms.DockStyle.Top;
-            this.metroTile_Exam.Location = new System.Drawing.Point(0, 430);
-            this.metroTile_Exam.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.metroTile_Exam.Name = "metroTile_Exam";
-            this.metroTile_Exam.Size = new System.Drawing.Size(200, 86);
-            this.metroTile_Exam.Style = MetroFramework.MetroColorStyle.Teal;
-            this.metroTile_Exam.TabIndex = 8;
-            this.metroTile_Exam.Text = "Luyện tập";
-            this.metroTile_Exam.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
-            this.metroTile_Exam.Theme = MetroFramework.MetroThemeStyle.Light;
-            this.metroTile_Exam.TileImage = ((System.Drawing.Image)(resources.GetObject("metroTile_Exam.TileImage")));
-            this.metroTile_Exam.TileImageAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.metroTile_Exam.TileTextFontSize = MetroFramework.MetroTileTextSize.Tall;
-            this.metroTile_Exam.TileTextFontWeight = MetroFramework.MetroTileTextWeight.Bold;
-            this.metroTile_Exam.UseSelectable = true;
-            this.metroTile_Exam.UseTileImage = true;
-            this.metroTile_Exam.Click += new System.EventHandler(this.metroTile_Exam_Click);
+            this.panel_Main.BackColor = System.Drawing.Color.White;
+            this.panel_Main.Location = new System.Drawing.Point(249, 74);
+            this.panel_Main.Name = "panel_Main";
+            this.panel_Main.Size = new System.Drawing.Size(624, 516);
+            this.panel_Main.TabIndex = 5;
             // 
-            // metroTile_Reading
+            // pictureBox_Search
             // 
-            this.metroTile_Reading.ActiveControl = null;
-            this.metroTile_Reading.Dock = System.Windows.Forms.DockStyle.Top;
-            this.metroTile_Reading.Location = new System.Drawing.Point(0, 344);
-            this.metroTile_Reading.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.metroTile_Reading.Name = "metroTile_Reading";
-            this.metroTile_Reading.Size = new System.Drawing.Size(200, 86);
-            this.metroTile_Reading.Style = MetroFramework.MetroColorStyle.Blue;
-            this.metroTile_Reading.TabIndex = 7;
-            this.metroTile_Reading.Text = "Bài đọc";
-            this.metroTile_Reading.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
-            this.metroTile_Reading.Theme = MetroFramework.MetroThemeStyle.Light;
-            this.metroTile_Reading.TileImage = ((System.Drawing.Image)(resources.GetObject("metroTile_Reading.TileImage")));
-            this.metroTile_Reading.TileImageAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.metroTile_Reading.TileTextFontSize = MetroFramework.MetroTileTextSize.Tall;
-            this.metroTile_Reading.TileTextFontWeight = MetroFramework.MetroTileTextWeight.Bold;
-            this.metroTile_Reading.UseSelectable = true;
-            this.metroTile_Reading.UseTileImage = true;
-            this.metroTile_Reading.Click += new System.EventHandler(this.metroTile_Reading_Click);
+            this.pictureBox_Search.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pictureBox_Search.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox_Search.Image")));
+            this.pictureBox_Search.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox_Search.InitialImage")));
+            this.pictureBox_Search.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox_Search.Name = "pictureBox_Search";
+            this.pictureBox_Search.Size = new System.Drawing.Size(81, 77);
+            this.pictureBox_Search.TabIndex = 0;
+            this.pictureBox_Search.TabStop = false;
+            this.pictureBox_Search.MouseEnter += new System.EventHandler(this.pictureBox_Search_MouseEnter);
+            this.pictureBox_Search.MouseLeave += new System.EventHandler(this.pictureBox_Search_MouseLeave);
             // 
-            // metroTile_Flashcard
+            // timer_PanelSearch
             // 
-            this.metroTile_Flashcard.ActiveControl = null;
-            this.metroTile_Flashcard.Dock = System.Windows.Forms.DockStyle.Top;
-            this.metroTile_Flashcard.Location = new System.Drawing.Point(0, 258);
-            this.metroTile_Flashcard.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.metroTile_Flashcard.Name = "metroTile_Flashcard";
-            this.metroTile_Flashcard.Size = new System.Drawing.Size(200, 86);
-            this.metroTile_Flashcard.Style = MetroFramework.MetroColorStyle.Teal;
-            this.metroTile_Flashcard.TabIndex = 6;
-            this.metroTile_Flashcard.Text = "Flash card";
-            this.metroTile_Flashcard.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
-            this.metroTile_Flashcard.Theme = MetroFramework.MetroThemeStyle.Light;
-            this.metroTile_Flashcard.TileImage = ((System.Drawing.Image)(resources.GetObject("metroTile_Flashcard.TileImage")));
-            this.metroTile_Flashcard.TileImageAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.metroTile_Flashcard.TileTextFontSize = MetroFramework.MetroTileTextSize.Tall;
-            this.metroTile_Flashcard.TileTextFontWeight = MetroFramework.MetroTileTextWeight.Bold;
-            this.metroTile_Flashcard.UseSelectable = true;
-            this.metroTile_Flashcard.UseTileImage = true;
-            this.metroTile_Flashcard.Click += new System.EventHandler(this.metroTile_Flashcard_Click);
+            this.timer_PanelSearch.Interval = 1;
+            this.timer_PanelSearch.Tick += new System.EventHandler(this.timer_PanelSearch_Tick);
             // 
-            // metroTile_ComboWords
+            // panel_Search
             // 
-            this.metroTile_ComboWords.ActiveControl = null;
-            this.metroTile_ComboWords.Dock = System.Windows.Forms.DockStyle.Top;
-            this.metroTile_ComboWords.Location = new System.Drawing.Point(0, 172);
-            this.metroTile_ComboWords.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.metroTile_ComboWords.Name = "metroTile_ComboWords";
-            this.metroTile_ComboWords.Size = new System.Drawing.Size(200, 86);
-            this.metroTile_ComboWords.Style = MetroFramework.MetroColorStyle.Blue;
-            this.metroTile_ComboWords.TabIndex = 5;
-            this.metroTile_ComboWords.Text = "Bộ từ vựng";
-            this.metroTile_ComboWords.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
-            this.metroTile_ComboWords.Theme = MetroFramework.MetroThemeStyle.Light;
-            this.metroTile_ComboWords.TileImage = ((System.Drawing.Image)(resources.GetObject("metroTile_ComboWords.TileImage")));
-            this.metroTile_ComboWords.TileImageAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.metroTile_ComboWords.TileTextFontSize = MetroFramework.MetroTileTextSize.Tall;
-            this.metroTile_ComboWords.TileTextFontWeight = MetroFramework.MetroTileTextWeight.Bold;
-            this.metroTile_ComboWords.UseSelectable = true;
-            this.metroTile_ComboWords.UseTileImage = true;
-            this.metroTile_ComboWords.Click += new System.EventHandler(this.metroTile_ComboWords_Click);
-            // 
-            // metroTile_Grammar
-            // 
-            this.metroTile_Grammar.ActiveControl = null;
-            this.metroTile_Grammar.Dock = System.Windows.Forms.DockStyle.Top;
-            this.metroTile_Grammar.Location = new System.Drawing.Point(0, 86);
-            this.metroTile_Grammar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.metroTile_Grammar.Name = "metroTile_Grammar";
-            this.metroTile_Grammar.Size = new System.Drawing.Size(200, 86);
-            this.metroTile_Grammar.Style = MetroFramework.MetroColorStyle.Teal;
-            this.metroTile_Grammar.TabIndex = 4;
-            this.metroTile_Grammar.Text = "Ngữ pháp";
-            this.metroTile_Grammar.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
-            this.metroTile_Grammar.Theme = MetroFramework.MetroThemeStyle.Light;
-            this.metroTile_Grammar.TileImage = ((System.Drawing.Image)(resources.GetObject("metroTile_Grammar.TileImage")));
-            this.metroTile_Grammar.TileImageAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.metroTile_Grammar.TileTextFontSize = MetroFramework.MetroTileTextSize.Tall;
-            this.metroTile_Grammar.TileTextFontWeight = MetroFramework.MetroTileTextWeight.Bold;
-            this.metroTile_Grammar.UseSelectable = true;
-            this.metroTile_Grammar.UseTileImage = true;
-            this.metroTile_Grammar.Click += new System.EventHandler(this.metroTile_Grammar_Click);
-            // 
-            // metroTile_Search
-            // 
-            this.metroTile_Search.ActiveControl = null;
-            this.metroTile_Search.BackColor = System.Drawing.Color.Yellow;
-            this.metroTile_Search.Dock = System.Windows.Forms.DockStyle.Top;
-            this.metroTile_Search.Location = new System.Drawing.Point(0, 0);
-            this.metroTile_Search.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.metroTile_Search.Name = "metroTile_Search";
-            this.metroTile_Search.Size = new System.Drawing.Size(200, 86);
-            this.metroTile_Search.Style = MetroFramework.MetroColorStyle.Blue;
-            this.metroTile_Search.TabIndex = 3;
-            this.metroTile_Search.Text = "Tra từ";
-            this.metroTile_Search.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
-            this.metroTile_Search.TileImage = ((System.Drawing.Image)(resources.GetObject("metroTile_Search.TileImage")));
-            this.metroTile_Search.TileImageAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.metroTile_Search.TileTextFontSize = MetroFramework.MetroTileTextSize.Tall;
-            this.metroTile_Search.TileTextFontWeight = MetroFramework.MetroTileTextWeight.Bold;
-            this.metroTile_Search.UseSelectable = true;
-            this.metroTile_Search.UseStyleColors = true;
-            this.metroTile_Search.UseTileImage = true;
-            this.metroTile_Search.Click += new System.EventHandler(this.metroTile_Search_Click);
-            // 
-            // metroPanel_Main
-            // 
-            this.metroPanel_Main.Dock = System.Windows.Forms.DockStyle.Left;
-            this.metroPanel_Main.HorizontalScrollbarBarColor = true;
-            this.metroPanel_Main.HorizontalScrollbarHighlightOnWheel = false;
-            this.metroPanel_Main.HorizontalScrollbarSize = 10;
-            this.metroPanel_Main.Location = new System.Drawing.Point(252, 74);
-            this.metroPanel_Main.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.metroPanel_Main.Name = "metroPanel_Main";
-            this.metroPanel_Main.Size = new System.Drawing.Size(624, 516);
-            this.metroPanel_Main.TabIndex = 3;
-            this.metroPanel_Main.VerticalScrollbarBarColor = true;
-            this.metroPanel_Main.VerticalScrollbarHighlightOnWheel = false;
-            this.metroPanel_Main.VerticalScrollbarSize = 11;
-            // 
-            // panel_Menu
-            // 
-            this.panel_Menu.Controls.Add(this.metroTile_Exam);
-            this.panel_Menu.Controls.Add(this.metroTile_Reading);
-            this.panel_Menu.Controls.Add(this.metroTile_Flashcard);
-            this.panel_Menu.Controls.Add(this.metroTile_ComboWords);
-            this.panel_Menu.Controls.Add(this.metroTile_Grammar);
-            this.panel_Menu.Controls.Add(this.metroTile_Search);
-            this.panel_Menu.Dock = System.Windows.Forms.DockStyle.Left;
-            this.panel_Menu.Location = new System.Drawing.Point(20, 74);
-            this.panel_Menu.Name = "panel_Menu";
-            this.panel_Menu.Size = new System.Drawing.Size(200, 516);
-            this.panel_Menu.TabIndex = 4;
-            // 
-            // panel_Selection
-            // 
-            this.panel_Selection.Dock = System.Windows.Forms.DockStyle.Left;
-            this.panel_Selection.Location = new System.Drawing.Point(220, 74);
-            this.panel_Selection.Name = "panel_Selection";
-            this.panel_Selection.Size = new System.Drawing.Size(32, 516);
-            this.panel_Selection.TabIndex = 5;
-            // 
-            // panel_SelectedCursor
-            // 
-            this.panel_SelectedCursor.Location = new System.Drawing.Point(230, 74);
-            this.panel_SelectedCursor.Name = "panel_SelectedCursor";
-            this.panel_SelectedCursor.Size = new System.Drawing.Size(12, 86);
-            this.panel_SelectedCursor.TabIndex = 0;
+            this.panel_Search.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(255)))), ((int)(((byte)(249)))));
+            this.panel_Search.Controls.Add(this.pictureBox_Search);
+            this.panel_Search.Location = new System.Drawing.Point(23, 74);
+            this.panel_Search.MaximumSize = new System.Drawing.Size(227, 77);
+            this.panel_Search.MinimumSize = new System.Drawing.Size(81, 77);
+            this.panel_Search.Name = "panel_Search";
+            this.panel_Search.Size = new System.Drawing.Size(81, 77);
+            this.panel_Search.TabIndex = 6;
             // 
             // Form_Main
             // 
@@ -245,11 +106,9 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BorderStyle = MetroFramework.Forms.MetroFormBorderStyle.FixedSingle;
             this.ClientSize = new System.Drawing.Size(896, 610);
-            this.Controls.Add(this.panel_SelectedCursor);
-            this.Controls.Add(this.metroPanel_Main);
-            this.Controls.Add(this.panel_Selection);
+            this.Controls.Add(this.panel_Search);
             this.Controls.Add(this.button_Exit);
-            this.Controls.Add(this.panel_Menu);
+            this.Controls.Add(this.panel_Main);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.MaximizeBox = false;
@@ -262,9 +121,9 @@
             this.Style = MetroFramework.MetroColorStyle.Default;
             this.Text = "Từ điển Anh-Việt";
             this.TransparencyKey = System.Drawing.Color.LightSteelBlue;
-            this.MouseLeave += new System.EventHandler(this.Form_Main_MouseLeave);
             ((System.ComponentModel.ISupportInitialize)(this.metroStyleManager_FormMain)).EndInit();
-            this.panel_Menu.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Search)).EndInit();
+            this.panel_Search.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -273,16 +132,10 @@
         private System.ComponentModel.BackgroundWorker backgroundWorker1;
         private MetroFramework.Components.MetroStyleManager metroStyleManager_FormMain;
         private System.Windows.Forms.Button button_Exit;
-        private MetroFramework.Controls.MetroTile metroTile_Search;
-        private MetroFramework.Controls.MetroTile metroTile_Exam;
-        private MetroFramework.Controls.MetroTile metroTile_Reading;
-        private MetroFramework.Controls.MetroTile metroTile_Flashcard;
-        private MetroFramework.Controls.MetroTile metroTile_ComboWords;
-        private MetroFramework.Controls.MetroTile metroTile_Grammar;
-        public MetroFramework.Controls.MetroPanel metroPanel_Main;
-        private System.Windows.Forms.Panel panel_Menu;
-        private System.Windows.Forms.Panel panel_Selection;
-        private System.Windows.Forms.Panel panel_SelectedCursor;
+        public System.Windows.Forms.Panel panel_Main;
+        private System.Windows.Forms.PictureBox pictureBox_Search;
+        private System.Windows.Forms.Timer timer_PanelSearch;
+        private System.Windows.Forms.Panel panel_Search;
     }
 }
 
