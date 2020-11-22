e844480229a1925d386ff0c188dbdb8e54f05647
ltl13 <66907671+ltl13@users.noreply.github.com>
2020-11-22 11:09:40 +0700

    asdf

--- GUI/UserControl_ComboWord.Designer.cs
+++ GUI/UserControl_ComboWord.Designer.cs
@@ -28,12 +28,30 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UserControl_ComboWord));
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Calibri", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(115, 35);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(354, 95);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Animal";
+            this.button1.UseVisualStyleBackColor = false;
             // 
             // UserControl_ComboWord
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.button1);
             this.Name = "UserControl_ComboWord";
             this.Size = new System.Drawing.Size(624, 516);
             this.ResumeLayout(false);
@@ -41,5 +59,7 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
