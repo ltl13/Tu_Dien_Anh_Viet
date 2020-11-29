21c347b803c461dc71724d1e36055fac1c7693c9
Ngô Dương Kha <57293030+ngoduongkha@users.noreply.github.com>
2020-11-29 20:58:42 +0700

    Revert "update"

--- GUI/UserControl_Translate.Designer.cs
+++ GUI/UserControl_Translate.Designer.cs
@@ -28,68 +28,40 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UserControl_Translate));
             this.richTextBox_From = new System.Windows.Forms.RichTextBox();
             this.richTextBox_To = new System.Windows.Forms.RichTextBox();
-            this.xuiButton_Translate = new XanderUI.XUIButton();
             this.SuspendLayout();
             // 
             // richTextBox_From
             // 
             this.richTextBox_From.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.richTextBox_From.Font = new System.Drawing.Font("Calibri", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.richTextBox_From.Location = new System.Drawing.Point(49, 49);
-            this.richTextBox_From.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.richTextBox_From.Location = new System.Drawing.Point(28, 27);
             this.richTextBox_From.Name = "richTextBox_From";
-            this.richTextBox_From.Size = new System.Drawing.Size(987, 361);
+            this.richTextBox_From.Size = new System.Drawing.Size(564, 199);
             this.richTextBox_From.TabIndex = 0;
             this.richTextBox_From.Text = "";
+            this.richTextBox_From.TextChanged += new System.EventHandler(this.richTextBox_From_TextChanged);
             // 
             // richTextBox_To
             // 
-            this.richTextBox_To.BackColor = System.Drawing.SystemColors.Window;
             this.richTextBox_To.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.richTextBox_To.Font = new System.Drawing.Font("Calibri", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.richTextBox_To.Location = new System.Drawing.Point(49, 522);
-            this.richTextBox_To.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.richTextBox_To.Location = new System.Drawing.Point(28, 288);
             this.richTextBox_To.Name = "richTextBox_To";
             this.richTextBox_To.ReadOnly = true;
-            this.richTextBox_To.Size = new System.Drawing.Size(987, 361);
+            this.richTextBox_To.Size = new System.Drawing.Size(564, 199);
             this.richTextBox_To.TabIndex = 1;
             this.richTextBox_To.Text = "";
             // 
-            // xuiButton_Translate
-            // 
-            this.xuiButton_Translate.BackgroundColor = System.Drawing.Color.White;
-            this.xuiButton_Translate.ButtonImage = ((System.Drawing.Image)(resources.GetObject("xuiButton_Translate.ButtonImage")));
-            this.xuiButton_Translate.ButtonStyle = XanderUI.XUIButton.Style.MacOS;
-            this.xuiButton_Translate.ButtonText = "Translate";
-            this.xuiButton_Translate.ClickBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(195)))), ((int)(((byte)(195)))), ((int)(((byte)(195)))));
-            this.xuiButton_Translate.ClickTextColor = System.Drawing.Color.DodgerBlue;
-            this.xuiButton_Translate.CornerRadius = 5;
-            this.xuiButton_Translate.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
-            this.xuiButton_Translate.Horizontal_Alignment = System.Drawing.StringAlignment.Center;
-            this.xuiButton_Translate.HoverBackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(225)))), ((int)(((byte)(225)))), ((int)(((byte)(225)))));
-            this.xuiButton_Translate.HoverTextColor = System.Drawing.Color.DodgerBlue;
-            this.xuiButton_Translate.ImagePosition = XanderUI.XUIButton.imgPosition.Left;
-            this.xuiButton_Translate.Location = new System.Drawing.Point(339, 425);
-            this.xuiButton_Translate.Name = "xuiButton_Translate";
-            this.xuiButton_Translate.Size = new System.Drawing.Size(355, 84);
-            this.xuiButton_Translate.TabIndex = 2;
-            this.xuiButton_Translate.TextColor = System.Drawing.Color.Black;
-            this.xuiButton_Translate.Vertical_Alignment = System.Drawing.StringAlignment.Center;
-            this.xuiButton_Translate.Click += new System.EventHandler(this.xuiButton_Translate_Click);
-            // 
             // UserControl_Translate
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.xuiButton_Translate);
             this.Controls.Add(this.richTextBox_To);
             this.Controls.Add(this.richTextBox_From);
-            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.Name = "UserControl_Translate";
-            this.Size = new System.Drawing.Size(1092, 935);
+            this.Size = new System.Drawing.Size(624, 516);
             this.ResumeLayout(false);
 
         }
@@ -98,6 +70,5 @@
 
         private System.Windows.Forms.RichTextBox richTextBox_From;
         private System.Windows.Forms.RichTextBox richTextBox_To;
-        public XanderUI.XUIButton xuiButton_Translate;
     }
 }
