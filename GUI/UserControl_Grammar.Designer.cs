3a2398b0e96e81309ac6bd859d1c0817777fbca7
ltl13 <66907671+ltl13@users.noreply.github.com>
2020-12-24 11:38:20 +0700

    A

--- GUI/UserControl_Grammar.Designer.cs
+++ GUI/UserControl_Grammar.Designer.cs
@@ -28,12 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(83, 39);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(448, 202);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(83, 283);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(448, 202);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // UserControl_Grammar
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
             this.Name = "UserControl_Grammar";
             this.Size = new System.Drawing.Size(624, 516);
             this.ResumeLayout(false);
@@ -41,5 +63,8 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
     }
 }
