e0bd2c432d068f178fccfe1705e654c182bd8708
ltl13 <66907671+ltl13@users.noreply.github.com>
2020-12-25 16:30:45 +0700

    Update UserControl_Grammar.cs

--- GUI/UserControl_Grammar.cs
+++ GUI/UserControl_Grammar.cs
@@ -1,14 +1,5 @@
 ﻿
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Drawing;
-using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Drawing.Text;
-using System.Runtime.InteropServices;
-
+using System.Windows.Forms;
 namespace GUI
 {
     public partial class UserControl_Grammar : UserControl
